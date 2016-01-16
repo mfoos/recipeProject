14aab3bff4b7ff560cc9bb574464c8dbecc60ac0
Marianna Foos <marianna.foos@gmail.com>
2016-01-15 21:54:25 -0500

    crawler works, output to stdout

--- webscraping/bc_rewrite.py
+++ webscraping/bc_rewrite.py
@@ -1,26 +1,47 @@
 from urllib.request import urlopen
 from bs4 import BeautifulSoup
+import requests
 import re
+import time
+import lxml
 
 ##################
-# This crawler gets the most recent "Business and Finance" articles
-# from the Brookings Institute, and prints out their title and lede
-# (or the first paragraph)
+# This script goes through the Budget Bytes archive by month and retrieves
+# ingredients and tags for each recipe, ignoring non-recipes. Data gets stored
+# in an XML file for later use.
 #################
 
-def getArticle(url):
+def getRecipe(url):
     print("URL: "+url)
-    #html = urlopen(url)
-    #articleObj = BeautifulSoup(html.read(), 'html.parser')
-    articleObj = BeautifulSoup(open("testBB.html"), 'html.parser')
+    time.sleep(1)
+    html = urlopen(url)
+    recipeObj = BeautifulSoup(html.read(), 'html.parser')
+    #recipeObj = BeautifulSoup(open("testBB.html"), 'html.parser')
 
-    #Get article title. This should have a class name ending in "title"
-    title = articleObj.find("h1", {"class":re.compile(".*title")}).get_text()
-    ingredients = [i.get_text() for i in articleObj.find_all("li",{"class":"ingredient"})]
-    tags = articleObj.findAll(attrs={"name":"shareaholic:keywords"})[0]['content'].split(',')
-    print(title, ingredients, tags)
+    title = recipeObj.find("h1", {"class":re.compile(".*title")}).get_text()
+    ingredients = [i.get_text() for i in recipeObj.find_all("li",{"class":"ingredient"})]
+    if not ingredients:
+        return
+    tags = recipeObj.findAll(attrs={"name":"shareaholic:keywords"})[0]['content'].split(',')
+    print(title) 
 
-# url = "http://www.budgetbytes.com/2015/12/slow-cooker-chicken-tikka-masala/"
-url = "xyz"
-getArticle(url)
 
+def getIndivUrls(baseUrl):
+    #for yr in range(2009, 2017):
+       #for mon in range(1,13):
+            #if (yr > 2009) | (mon > 4):
+                yr = 2015
+                mon = 6
+                archivUrl = baseUrl + str(yr) + "/" + str(format(mon,'02d')) + "/"
+                html = urlopen(archivUrl)
+                recipeObj = BeautifulSoup(html.read(), 'html.parser')
+                allUrls = recipeObj.findAll("a",{"rel":"bookmark"})
+                allLinks = [i.get('href') for i in allUrls]
+                for link in allLinks:
+                    getRecipe(link)
+
+def writeXML(url, title, ingreds, tags):
+    
+                    
+baseUrl = "http://www.budgetbytes.com/"
+getIndivUrls(baseUrl)
